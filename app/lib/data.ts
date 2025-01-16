e84306761fd1bf6f15ed8823772a91e8867c5c66
Brian Cantrell <bribri546@gmail.com>
2025-01-15 21:33:36 -0700

    Dummy data for wireframing

---
+++ app/lib/data.ts
@@ -0,0 +1,69 @@
+import { mockCourses } from "./dummy";
+
+export type Course = {
+  id: string;
+  name: string;
+	description: string;
+  courseProgress: {
+    currentUnit: number;
+    totalUnits: number;
+  };
+  units: Unit[];
+};
+
+export type Unit = {
+  id: string;
+  title: string;
+  status: 'not-started' | 'in-progress' | 'complete';
+  progress: number;
+  sessions: Session[];
+};
+
+export type Session = {
+  id: string;
+  title: string;
+  duration: string;
+  status: 'not-started' | 'in-progress' | 'complete';
+  activities: Activity[];
+};
+
+export type Activity = {
+	id: string;
+	title: string;
+	duration: string;
+	status: 'not-started' | 'in-progress' | 'complete';
+	imgUrl?: string;
+	description?: string;
+	questions?: Questions[];
+}
+
+export type Questions = {
+	id: string;
+	question: string;
+	correct: string;
+	options: AnswerOption[]
+}
+
+export type AnswerOption = {
+	id: string;
+	text: string;
+}
+
+export async function getCourse(courseId: string): Promise<Course | undefined> {
+  return mockCourses.find(course => course.id === courseId);
+}
+
+export async function getSession(courseId: string, sessionId: string): Promise<Session | undefined> {
+  const course = await getCourse(courseId);
+  return course?.units
+    .flatMap(unit => unit.sessions)
+    .find(session => session.id === sessionId);
+}
+
+export async function getActivities(
+  courseId: string,
+  sessionId: string
+): Promise<Activity[] | undefined> {
+  const session = await getSession(courseId, sessionId);
+  return session?.activities;
+}
